0200f66097fcf0ac9979aed09bba7f6b857c94c5
igorkorobko <nekto.korobko@gmail.com>
2023-09-10 16:23:02 +0300

    Distance to restaurants into manager UI

--- star_burger/settings.py
+++ star_burger/settings.py
@@ -14,6 +14,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 SECRET_KEY = env('SECRET_KEY')
 DEBUG = env.bool('DEBUG', True)
+YANDEX_TOKEN = env('YANDEX_TOKEN')
 
 ALLOWED_HOSTS = env.list('ALLOWED_HOSTS', ['127.0.0.1', 'localhost'])
 
