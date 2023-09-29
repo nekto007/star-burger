7328681eb7806ad7ab10136d0ccd3fc4379198a9
igorkorobko <nekto.korobko@gmail.com>
2023-09-29 03:38:48 +0300

    create deploy_star_burger.sh

---
+++ deploy_star_burger.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+set -e
+source .env
+echo "Check for updates on remote git repository."
+git pull
+
+echo "Install Python dependencies."
+./venv/bin/pip install -r requirements.txt
+
+echo "Collect Django static."
+./venv/bin/python3 manage.py collectstatic --noinput
+
+echo "Apply Django migrations."
+./venv/bin/python3 manage.py migrate --noinput
+
+echo "Restart systemd services."
+systemctl restart star-burger.service
+systemctl restart postgresql.service
+systemctl reload nginx.service
+
+echo "Send deployment info to Rollbar."
+sha=$(git rev-parse HEAD)
+curl --request POST \
+     --url https://api.rollbar.com/api/1/deploy \
+     --header "X-Rollbar-Access-Token: ${ROLLBAR_TOKEN}" \
+     --header 'accept: application/json' \
+     --header 'content-type: application/json' \
+     --data '
+              {
+                "environment": "'deployment'",
+                "local_username": "'"${USER}"'",
+                "revision": "'"${sha}"'",
+                "status": "succeeded"
+              }
+            '
+
+echo "Deployment completed!"
